d2e1d1d7deea846f7132f4832046f5e322020447
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-04-25 02:07:45 +0900

    Add image by Picasso.

--- app/src/main/java/com/sadashi/playground/ui/picasso/PicassoActivity.kt
+++ app/src/main/java/com/sadashi/playground/ui/picasso/PicassoActivity.kt
@@ -1,13 +1,21 @@
 package com.sadashi.playground.ui.picasso
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.sadashi.playground.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_picasso.*
 
 class PicassoActivity : AppCompatActivity() {
+
+    companion object {
+        private const val imageUrl = "https://avatars0.githubusercontent.com/u/29793443?s=460&v=4"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picasso)
+
+        Picasso.with(this).load(imageUrl).into(imageView)
     }
 }
